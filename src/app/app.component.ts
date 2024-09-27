f6cc94f2dfe98a2fe8dbae6f9e829540b874fc5c
AmanJais0660 <amanjaiswal660.aj@gmail.com>
2024-09-28 01:33:51 +0530

    some fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root1',
   templateUrl: './app.component.html',
-  standalone: true
 })
 export class AppComponent {
 
   title = 'Custom Timepicker Demo';
   clockIn: any = { hour: 3, minute: 50, second: 0 };
   meredian: boolean = true;
-  name: any = "aman"
-  
+
 }
 
